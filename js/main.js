2a11fadc9105355a4e1649c0605a16704c3a6c1d
Taryn <taryn@sugarpush.com.au>
2018-05-12 16:54:10 +1000

    request for json

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const requestURL = 'https://raw.githubusercontent.com/finderau/recruitment/master/front-end/design-and-js/01-page-layout-listing/data.json';
+let article;
+let story;
+let header;
+
+const request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+  console.log(request.response);
+  const results = request.response;
+  populateArticles(results);
+}
+
+function populateArticles(jsonObj) {
+articles = document.querySelector('#articles')
+
+  for (let i = 0; i < 5; i++) {
+    container = document.createElement('div');
+    container.setAttribute('class', 'individual-post');
+    story = jsonObj[i];
+
+    header = document.createElement('a');
+    header.innerHTML = `${story.title}`;
+    header.setAttribute('href', `${story.url}`);
+    snippet = document.createElement('p');
+    snippet.innerText = `${story.description}`;
+
+    container.appendChild(header);
+    container.appendChild(snippet);
+    articles.appendChild(container);
+  }
+
+}
